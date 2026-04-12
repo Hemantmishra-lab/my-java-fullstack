1ea9085078f6f78d6d002aaeb30afdd89d87d167
Hemantmishra-lab <hemantmishra19781978@gmail.com>
2026-04-12 10:16:09 +0530

    Model View Controller DAO Service

--- trumio-spring-mvc/src/main/java/com/trumio/controller/AdmissionController.java
+++ trumio-spring-mvc/src/main/java/com/trumio/controller/AdmissionController.java
@@ -11,7 +11,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;//this is import important
 
+
+
+// with Redirect prefix total request = 2
 @Controller
 public class AdmissionController {
 
@@ -23,23 +27,67 @@ public class AdmissionController {
         return "admission-form";
     }
 
-    @RequestMapping("/processAdmission")
+    @PostMapping("/processAdmission")
     public String handleForm(
             @RequestParam("sId") int id,
             @RequestParam("sName") String name,
             @RequestParam("sCourse") String course,
             @RequestParam("sSubjects") List<String> subjects,
-            Model model) {
+            RedirectAttributes redirectAttributes) { //Instead of using Model ue RedirectAttribute interface of spring mvc
 
-        // Model Object creation
+        // 1. Create the Object
         Student studentObj = new Student(id, name, course, subjects);
 
-        // Saving to DB via Service
+        // 2. Save in the Database
         studentService.registerStudent(studentObj);
 
-        // Adding to Spring Model for View
-        model.addAttribute("st", studentObj);
+        // 3. Adding Flash Attribute(Magic)
+        // Put the name as "st" so we can extract in jsp
+        redirectAttributes.addFlashAttribute("st", studentObj);
 
+        // 4. Redirect in new URL
+        return "redirect:/admission-success";
+    }
+
+    // New Handler after redirecting
+    @RequestMapping("/admission-success")
+    public String showSuccessPage() {
+        //Here 'st'  will be found in the model automatically due to the spring
         return "confirm-page";
     }
 }
+
+
+
+//without any redirect total request = 1
+//@Controller
+//public class AdmissionController {
+//
+//    @Autowired
+//    private StudentService studentService;
+//
+//    @RequestMapping("/admission")
+//    public String showForm() {
+//        return "admission-form";
+//    }
+//
+//    @RequestMapping("/processAdmission")
+//    public String handleForm(
+//            @RequestParam("sId") int id,
+//            @RequestParam("sName") String name,
+//            @RequestParam("sCourse") String course,
+//            @RequestParam("sSubjects") List<String> subjects,
+//            Model model) {
+//
+//        // Model Object creation
+//        Student studentObj = new Student(id, name, course, subjects);
+//
+//        // Saving to DB via Service
+//        studentService.registerStudent(studentObj);
+//
+//        // Adding to Spring Model for View
+//        model.addAttribute("st", studentObj);
+//
+//        return "confirm-page";
+//    }
+//}
